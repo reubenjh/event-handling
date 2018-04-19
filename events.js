8f154465209db33961ecc8f32bb3bc4a9cd33f69
reubenjh <rjharcourt@gmail.com>
2018-04-20 11:29:55 +1200

    div 3

--- events.js
+++ events.js
@@ -8,6 +8,7 @@ function start () {
   // when you mouse over it.
   one()
   two()
+  three()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -33,6 +34,12 @@ function two () {
 
 // CREATE FUNCTION three HERE
 
+function three () {
+  var three = document.getElementById('three');
+  three.addEventListener('mouseenter', makeOrange);
+  three.addEventListener('mouseleave', makeWhite);
+}
+
 // CREATE FUNCTION four HERE
 
 // Changes the background color of event's target
@@ -46,4 +53,8 @@ function makeWhite (evt) {
 
 function makeGreen (evt) {
   evt.target.style.backgroundColor = 'green';
+}
+
+function makeOrange (evt) {
+  evt.target.style.backgroundColor = 'orange';
 }
